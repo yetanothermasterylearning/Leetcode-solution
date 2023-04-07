db0b41c420858b60bdd178a8b245aa0d2579147e
yetanothermasterylearning <106942073+yetanothermasterylearning@users.noreply.github.com>
2023-04-06 20:35:15 -0700

    Time: 0 ms (100.00%), Space: 40.7 MB (31.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
+        char[] tempArray = new char[s.length()];
+        int index = 0;
+        
+        for (char c:s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {//save
+                tempArray[index] = c;
+                index++;
+            } else {
+                if (index == 0) {
+                    return false;
+                }
+                index--;
+                char latestBracket = tempArray[index];
+                if ((c == ')' && latestBracket != '(') || 
+                    (c == ']' && latestBracket != '[') ||
+                    (c == '}' && latestBracket != '{')) {
+                    return false;
+                }
+            }
+        }
+        return index == 0;
+        /*
         Stack<Character> stack = new Stack<>();
         for (char c:s.toCharArray()) {
             
@@ -18,5 +40,6 @@ class Solution {
             }
         }
         return stack.isEmpty();
+        */
     }
 }
